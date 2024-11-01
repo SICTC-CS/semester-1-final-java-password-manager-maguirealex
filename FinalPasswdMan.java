060dabc79643c9489cc8ac12665c178d98778884
Maguire <136644201+LeopardDrager@users.noreply.github.com>
2024-11-01 13:18:50 -0500

    Add files via upload

--- FinalPasswdMan.java
+++ FinalPasswdMan.java
@@ -1,5 +1,74 @@
+import java.util.Scanner;
+
 public class FinalPasswdMan {
     public static void main(args[] String){
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
         
+        while(run)
+        
+        printingMenu();
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        switch (choice) {
+            case 1:
+                //add Account
+                break;
+            case 2:
+                //view Account
+                break;
+            case 3:
+                //modify Account
+                break;
+
+            case 4:
+                //remove account           
+                break;
+
+            case 5:
+                //save and exit 
+                break;
+            
+            case 6:
+                run = false;
+                break;
+        }
+
+
     }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static void printingMenu() {
+        System.out.println("\n=== Password Manager ===");
+        System.out.println("1. Add Account");
+        System.out.println("2. View Account");
+        System.out.println("3. Modify Account");
+        System.out.println("4. Remove Account");
+        System.out.println("5. Save and Close");
+        System.out.println("6. Exit");
+    }
+
+
 }
